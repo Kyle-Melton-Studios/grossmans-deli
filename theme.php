26fff4854bc6b386e82278609abf467193663b76
km321 <65635684+km321@users.noreply.github.com>
2021-01-28 18:42:16 -0500

    Update theme.php

--- theme.php
+++ theme.php
@@ -62,7 +62,7 @@
 						</ul>
 				</div>
 		</nav>
-		<?php 
+
 		if(location.pathname == '/') {
 			<div class="container centered-text">
 				<img class="deli-logo" src="img/deli.png" width="300px">
@@ -90,7 +90,7 @@
 } else {
 
 }
-?>
+
 
 		<section class="container mt-5 mb-5">
 			<div class="row">
